9fa089e973b6e1b1b0f010b50484e130a2506022
Claire Gaestel <213631+nyobe@users.noreply.github.com>
2025-03-24 09:39:15 -0700

    support building multiple archives

--- .ci/index.ts
+++ .ci/index.ts
@@ -6,7 +6,7 @@ import * as child_process from "child_process";
 
 const config = new pulumi.Config()
 const distributionRegions = config.getObject<aws.Region[]>("distributionRegions") || []
-const githash = child_process.execFileSync("git", ["log", "-n1", "--pretty=%H", "../rotators"], {encoding: "utf8"}).trim()
+const lambdasToBuild = ["aws-lambda"];
 
 // Create a signing profile for stamping the lambda archives.
 const signingProfile = new aws.signer.SigningProfile("signing-profile", {
@@ -16,27 +16,6 @@ const signingProfile = new aws.signer.SigningProfile("signing-profile", {
 
 // Use a code signing config referencing this arn to verify lambda archives.
 export const signingProfileVersionArn = signingProfile.versionArn;
-
-// Build & upload the lambda archives.
-// Use a docker container to build the lambda archive for toolchain consistency.
-const builder = new dockerbuild.Image("lambda-builder", {
-    context: {
-        location: "../rotators/aws-lambda",
-    },
-    buildArgs: {
-        OUTFILE_FILENAME: "lambda-deployment-package.zip",
-    },
-    exports: [{
-        local: {dest: "./build"},
-    }],
-    // need to build on preview to successfully calculate an asset hash
-    buildOnPreview: true,
-    push: false,
-})
-
-const archive = pulumi.all([builder.buildArgs, builder.exports]).apply(([args, exports]) => {
-    return new pulumi.asset.FileAsset(path.join(exports![0].local!.dest, args!["OUTFILE_FILENAME"]))
-})
 
 const stagingBucket = new aws.s3.BucketV2(`esc-rotator-lambdas-${pulumi.getStack()}`, {
     forceDestroy: true,
@@ -49,35 +28,62 @@ const stagingBucketVersioning = new aws.s3.BucketVersioningV2("staging-bucket-ve
     }
 })
 
-const object = new aws.s3.BucketObjectv2("lambda-staged.zip", {
-    bucket: stagingBucket.bucket,
-    key: pulumi.interpolate`staged/${githash}.zip`,
-    source: archive,
-}, {retainOnDelete: true})
+// Build & upload the lambda archives.
+const signedArchives = lambdasToBuild.map(name => {
+    const githash = child_process.execFileSync("git", ["log", "-n1", "--pretty=%H", `../rotators/${name}`], {encoding: "utf8"}).trim()
 
-const signingJob = new aws.signer.SigningJob("lambda-signed.zip", {
-    profileName: signingProfile.name,
-    source: {
-        s3: {
-            bucket: object.bucket,
-            key: object.key,
-            version: object.versionId,
+    // Use a docker container to build the lambda archive for toolchain consistency.
+    const builder = new dockerbuild.Image(`${name}-lambda-builder`, {
+        context: {
+            location: `../rotators/${name}`,
         },
-    },
-    destination: {
-        s3: {
-            bucket: stagingBucket.bucket,
-            prefix: "signed/"
-        }
-    },
-    ignoreSigningJobFailure: false,
-}, {dependsOn: [stagingBucketVersioning]})
+        buildArgs: {
+            OUTFILE_FILENAME: "lambda-deployment-package.zip",
+        },
+        exports: [{
+            local: {dest: `./build/${name}`},
+        }],
+        // need to build on preview to successfully calculate an asset hash
+        buildOnPreview: true,
+        push: false,
+    })
 
-const signedArchive = signingJob.signedObjects[0].s3s[0];
+    const archive = pulumi.all([builder.buildArgs, builder.exports]).apply(([args, exports]) => {
+        return new pulumi.asset.FileAsset(path.join(exports![0].local!.dest, args!["OUTFILE_FILENAME"]))
+    })
+
+    const object = new aws.s3.BucketObjectv2(`${name}-lambda-staged.zip`, {
+        bucket: stagingBucket.bucket,
+        key: pulumi.interpolate`staged/${name}/${githash}.zip`,
+        source: archive,
+    }, {retainOnDelete: true})
+
+    const signingJob = new aws.signer.SigningJob(`${name}-lambda-signed.zip`, {
+        profileName: signingProfile.name,
+        source: {
+            s3: {
+                bucket: object.bucket,
+                key: object.key,
+                version: object.versionId,
+            },
+        },
+        destination: {
+            s3: {
+                bucket: stagingBucket.bucket,
+                prefix: `signed/${name}/`
+            }
+        },
+        ignoreSigningJobFailure: false,
+    }, {dependsOn: [stagingBucketVersioning]})
+
+    const signedArchive = signingJob.signedObjects[0].s3s[0];
+
+    return {name: name, signedArchive: signedArchive};
+});
 
 // Set up distribution buckets.
 // Lambda can only update code from a s3 bucket within its same region, so create buckets in each region to replicate the archives to.
-export const distributionBuckets = distributionRegions.map((region, index) => {
+export const distributions = distributionRegions.flatMap((region) => {
     const provider = new aws.Provider(`${region}-provider`, {
         region: region,
     });
@@ -129,12 +135,14 @@ export const distributionBuckets = distributionRegions.map((region, index) => {
         }
     }, {...opts, dependsOn: [allowPublicPolicy]});
 
-    // Replicate the signed archive to this region
-    const replica = new aws.s3.ObjectCopy(`${region}-lambda-latest.zip`, {
-        source: pulumi.interpolate`${signedArchive.bucket}/${signedArchive.key}`,
-        bucket: distBucket.bucket,
-        key: "latest.zip",
-    }, {...opts, retainOnDelete: true})
+    // Replicate the signed archives to this region
+    const replicas = signedArchives.map(archive => {
+        return new aws.s3.ObjectCopy(`${archive.name}-lambda-latest.zip`, {
+            source: pulumi.interpolate`${archive.signedArchive.bucket}/${archive.signedArchive.key}`,
+            bucket: distBucket.bucket,
+            key: `${archive.name}/latest.zip`,
+        }, {...opts, retainOnDelete: true})
+    })
 
-    return pulumi.interpolate`https://${distBucket.bucketDomainName}/${replica.key}`
+    return replicas.map(replica => pulumi.interpolate`https://${distBucket.bucketDomainName}/${replica.key}`)
 });
